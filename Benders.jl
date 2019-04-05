3ec65d5f0e240e4cc742dda04ba368d37d9b7013
edxu96 <edxu96@outlook.com>
2019-04-05 21:44:15 +0200

    Update

--- Benders.jl
+++ Benders.jl
@@ -1,6 +1,7 @@
 # Benders Decomposition
 # 1. BendersOptim.milp: for MILP with Sub and Ray Problems
 # 2. BendersOptim.lshaped: for stochastic programming
+# 0. Linear programming in matrix
 # Version: 5.1
 # Author: Edward J. Xu, edxu96@outlook.com
 # Date: April 5th, 2019
@@ -12,4 +13,6 @@ module Benders
     include("Benders_milp.jl")
     # 2. BendersOptim.lshaped: for stochastic programming
     include("Benders_lshaped.jl")
+    # 0. Linear programming
+    include("lp.jl")
 end
