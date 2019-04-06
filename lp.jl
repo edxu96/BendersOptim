1cc9b46c3837ba1041d89c2b1ff68243582fa8b6
edxu96 <edxu96@outlook.com>
2019-04-06 22:07:14 +0200

    Update

--- lp.jl
+++ lp.jl
@@ -5,11 +5,16 @@
 # ----------------------------------------------------------------------------------------------------------------------
 # using JuMP
 # using GLPKMathProgInterface
-function lp(; n_x, vec_c, vec_b, mat_a)
+function lp(n_x, vec_c, vec_b, mat_a)
+    # Linear Programming in Matrix Form
+    # vec_c: [column vector] coefficient vector in objective function
+    # vec_b: [column vector] right-hand-side coefficient vector in constraint
+    # mat_a:
+    # Edward J. Xu, 190405
     println("-------------------------------------------------------------------------\n",
             "------------------------ 1/2. Begin Optimization ------------------------\n",
             "-------------------------------------------------------------------------\n")
-    model = Model(solver = GLPKSolverMIP())
+    model = Model(solver = GLPKSolverLP())
     @variable(model, vec_x[1: n_x] >= 0)
     @objective(model, Min, (transpose(vec_c) * vec_x)[1])
     constraintsForDual = @constraint(model, mat_a * vec_x .>= vec_b)
